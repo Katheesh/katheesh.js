3e9543f1f90b799efd9c41aacaee77d7684a4433
Katheeskumar Sithamparapillai <katheeskumar@outlook.com>
2020-06-19 22:41:59 +0530

    added AOS animate package

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Toasted from 'vue-toasted';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 Vue.use(Toasted)
 Vue.config.productionTip = false;
 
 
 new Vue({
+  created () {
+    AOS.init()
+  },
   router,
   store,
   render: h => h(App)
